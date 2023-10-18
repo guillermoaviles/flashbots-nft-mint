49cbbf83eaf094b698e8f6822e194d6bfe6bb275
Guillermo Aviles <gjaviles7@gmail.com>
2023-10-17 23:05:38 -0400

    flashbots provider

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { ethers } from "ethers";
+import { ethers, Wallet } from "ethers";
+import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle"
 
 const provider = new ethers.providers.AlchemyProvider(
   "goerli",
   "X-wCXLJfkRiiu676ApR7wnV5FfoNQu3m"
 );
 
+const FLASHBOTS_ENDPOINT = "https://relay-goerli.flashbots.net";
+
 async function main() {
-  console.log(await provider.getBlockNumber());
+    const flashbotsProvider = await FlashbotsBundleProvider.create(provider, Wallet.createRandom(), FLASHBOTS_ENDPOINT)
+    provider.on('block', (blockNumber) => {
+        console.log(blockNumber);
+    })
 }
 
 main();
